70f755a170ce1c8c89e9aa766435c94ae3623ab4
Darren Poon <dyhpoon@gmail.com>
2017-10-26 18:56:01 +0800

    add more mediator pattern

--- 21-mediator/3-betterTeam.js
+++ 21-mediator/3-betterTeam.js
@@ -36,5 +36,78 @@ var playerDirector = function() {
 
     operations.addPlayer = function(player) {
         var teamColor = player.teamColor
+        players[teamColor] = players[teamColor] || []
+        players[teamColor].push(player)
+    }
+
+    operations.removePlayer = function(player) {
+        var teamColor = player.teamColor,
+        teamPlayers = players[teamColor] || []
+
+        for (var i = teamPlayers.length - 1; i >= 0; i--) {
+            if (teamPlayers[i] === player) {
+                teamPlayers.splice(i, 1)
+            }
+        }
+    }
+
+    operations.changeTeam = function(player, newTeamColor) {
+        operations.removePlayer(player)
+        player.teamColor = newTeamColor
+        operations.addPlayer(player)
+    }
+
+    operations.playerDead = function(player) {
+        var teamColor = player.teamColor
+        , teamPlayers = players[teamColor]
+
+        var all_dead = true
+
+        for (var i = 0, player; player = teamPlayers[i++];) {
+            if (player.state !== 'dead') {
+                all_dead = false
+                break
+            }
+        }
+
+        if (all_dead === true) {
+            for (var i = 0, player; player = teamPlayers[i++];) {
+                player.lose()
+            }
+
+            for (var color in players) {
+                if (color !== teamColor) {
+                    var teamPlayers = players[color]
+                    for (var i = 0, player; player = teamPlayers[i++];) {
+                        player.win()
+                    }
+                }
+            }
+        }
+    }
+
+    var ReceiveMessage = function() {
+        var message = Array.prototype.shift.call(arguments)
+        operations[message].apply(this, arguments)
+    }
+
+    return {
+        ReceiveMessage
     }
 }()
+
+var player1 = playerFactory('player1', 'red')
+, player2 = playerFactory('player2', 'red')
+, player3 = playerFactory('player3', 'red')
+, player4 = playerFactory('player4', 'red')
+
+var player5 = playerFactory('player5', 'blue')
+, player6 = playerFactory('player6', 'blue')
+, player7 = playerFactory('player7', 'blue')
+, player8 = playerFactory('player8', 'blue')
+
+
+player1.die()
+player2.die()
+player4.die()
+player3.die()
