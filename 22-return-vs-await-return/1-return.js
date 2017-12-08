2f66ea6950cce4f153cc32c92954cdb55b1a58da
Darren Poon <dyhpoon@gmail.com>
2017-12-08 22:45:48 +0800

    add return vs await-return

---
+++ 22-return-vs-await-return/1-return.js
@@ -0,0 +1,23 @@
+async function waitAndMaybeReject() {
+    // Wait one second
+    await new Promise(r => setTimeout(r, 1000));
+    // Toss a coin
+    const isHeads = Boolean(Math.round(Math.random()));
+  
+    if (isHeads) return 'yay';
+    throw Error('Boo!');
+}
+
+async function foo() {
+    try {
+      return waitAndMaybeReject();
+    }
+    catch (e) {
+      return 'caught';
+    }
+}
+
+(async () => {
+    const result = await foo()
+    console.log(result)
+})()
